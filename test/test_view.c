98a40710fee2615731f49e356e265816d21f6e3a
Wayne Booth <wbooth@clearscene.net>
2017-06-27 23:10:44 +0100

    Final sily view test for tonight

--- test/test_view.c
+++ test/test_view.c
@@ -2,6 +2,20 @@
 #include "../src/view.h"
 #include "../src/model.h"
 
+void drawChip(void);
+void drawOutput( int, float, float, int );
+void drawInputs( int, float, float, int );
+
+MU_TEST( view___draw_outputs ) {
+	drawOutput( 1, 0.1F, 0.1F, 1 );
+	mu_assert_uint32_eq( 1, 1 );
+}
+
+MU_TEST( view___draw_inputs ) {
+	drawInputs( 1, 0.1F, 0.1F, 1 );
+	mu_assert_uint32_eq( 1, 1 );
+}
+
 MU_TEST( view___draw_chip ) {
 	drawChip();
 	mu_assert_uint32_eq( 1, 1 );
@@ -20,5 +34,7 @@ MU_TEST_SUITE( test_view ) {
 
 	CHIPNAME = getChipName;
 	MU_RUN_TEST( view___draw_chip );
+	MU_RUN_TEST( view___draw_outputs );
+	MU_RUN_TEST( view___draw_inputs );
 	MU_RUN_TEST( view_pass );
 }
