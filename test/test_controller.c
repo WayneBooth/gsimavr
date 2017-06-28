07b4ac11b2613b45a01af40bf95ec3c42af548e3
Wayne Booth <wbooth@clearscene.net>
2017-06-28 13:07:05 -0700

    Add simple controller test

--- test/test_controller.c
+++ test/test_controller.c
@@ -1,11 +1,12 @@
 #include "minunit.h"
 #include "../src/controller.h"
 
-MU_TEST( controller_pass ) {
+MU_TEST( controller___setupConnectivity ) {
+	setupConnectivity();
 	mu_assert_uint32_eq( 1, 1 );
 }
 
 MU_TEST_SUITE( test_controller ) {
 
-	MU_RUN_TEST( controller_pass );
+	MU_RUN_TEST( controller___setupConnectivity );
 }
