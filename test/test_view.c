9aece0e52f62281eb3199d515576d8df2cb6e330
Wayne Booth <wbooth@clearscene.net>
2017-06-28 12:55:33 -0700

    add 'drawWire' 'view' test

--- test/test_view.c
+++ test/test_view.c
@@ -31,6 +31,11 @@ MU_TEST( view___renderScene ) {
 }
 
 
+MU_TEST( view___draw_wire ) {
+	drawWire( 1, 0.2F, 0.2F, 1 );
+	mu_assert_uint32_eq( 1, 1 );
+}
+
 MU_TEST( view___draw_chip ) {
 	drawChip();
 	mu_assert_uint32_eq( 1, 1 );
@@ -56,6 +61,7 @@ MU_TEST_SUITE( test_view ) {
 
 	MU_RUN_TEST( view___renderScene );
 
+	MU_RUN_TEST( view___draw_wire );
 	MU_RUN_TEST( view___draw_chip );
 	MU_RUN_TEST( view___draw_outputs );
 	MU_RUN_TEST( view___draw_inputs );
