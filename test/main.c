423d3fa7f16c78f411be3f09023259afddb14c68
Wayne Booth <wbooth@clearscene.net>
2017-07-02 13:59:07 -0700

    Make sure ac_input is fully tested (no need for a dedicated test for this module)

--- test/main.c
+++ test/main.c
@@ -17,7 +17,6 @@ int main(int argc, char *argv[]) {
 	MU_RUN_SUITE( test_view );
 	MU_RUN_SUITE( test_model );
 	MU_RUN_SUITE( test_controller );
-	MU_RUN_SUITE( test_ac_input );
 
 	MU_REPORT();
 	if (!minunit_status && minunit_fail) { return 1; }
